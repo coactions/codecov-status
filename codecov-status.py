a53293dac4598aa268b7426b1f8924a2b2ee4c74
Sorin Sbarnea <ssbarnea@redhat.com>
2024-02-09 17:04:06 +0000

    Enable pre-commit (#2)

--- codecov-status.py
+++ codecov-status.py
@@ -9,15 +9,21 @@ from urllib.request import urlopen
 
 if __name__ == "__main__":
 
-    if os.environ.get("GITHUB_REPOSITORY", "") and os.environ.get("GITHUB_REPOSITORY", ""):
+    if os.environ.get("GITHUB_REPOSITORY", "") and os.environ.get(
+        "GITHUB_REPOSITORY", ""
+    ):
         org, repo = os.environ.get("GITHUB_REPOSITORY", "").split("/")
         pr = os.environ.get("GITHUB_REF_NAME", "").split("/")[0]
     else:
-        print("::warning::GITHUB_REPOSITORY and GITHUB_REF_NAME are needed to determine current pull request.")
+        print(
+            "::warning::GITHUB_REPOSITORY and GITHUB_REF_NAME are needed to determine current pull request."
+        )
         sys.exit(0)
 
     if not pr.isnumeric():
-        print("::warning::Codecov.io status check skipped because a pull request was not detected.")
+        print(
+            "::warning::Codecov.io status check skipped because a pull request was not detected."
+        )
         sys.exit(0)
 
     url = f"https://api.codecov.io/api/v2/github/{org}/repos/{repo}/pulls/{pr}/"
@@ -35,6 +41,10 @@ if __name__ == "__main__":
     msg = f"{abs(delta_coverage):.2f}% ({base_cov:.2f}% on base -> {head_cov:.2f}% on head).\n"
     msg += f"See https://app.codecov.io/gh/{org}/{repo}/pull/{pr} for details."
     if delta_coverage < 0:
-        print(f"::error::Setting coverage check as failed due to coverage going down by {msg}")
+        print(
+            f"::error::Setting coverage check as failed due to coverage going down by {msg}"
+        )
         sys.exit(2)
-    print(f"::notice::Setting coverage check as passed due to coverage going up by {msg}")
+    print(
+        f"::notice::Setting coverage check as passed due to coverage going up by {msg}"
+    )
