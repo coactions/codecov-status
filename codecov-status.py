df99dd744935a6d3a5fc90e2ee4ea5db0890e81d
Sorin Sbarnea <ssbarnea@redhat.com>
2024-05-20 15:24:30 +0100

    Implement retry logic for API calls that return no head_totals

--- codecov-status.py
+++ codecov-status.py
@@ -4,6 +4,7 @@ import json
 import os
 import sys
 
+import time
 from urllib.request import urlopen
 
 
@@ -26,19 +27,29 @@ if __name__ == "__main__":
         )
         sys.exit(0)
 
-    url = f"https://api.codecov.io/api/v2/github/{org}/repos/{repo}/pulls/{pr}/"
-    print(f"Getting codecov.io status from {url}")
-    with urlopen(url) as response:
-        response_content = response.read().decode("utf-8")
-        data = json.loads(response_content)
-        print(data, file=sys.stdout)
-        base_cov = 0.0
-        head_cov = 0.0
-        if data["base_totals"] is not None:
-            base_cov = data["base_totals"]["coverage"]
-        if data["head_totals"] is not None:
-            head_cov = data["head_totals"]["coverage"]
-        delta_coverage = head_cov - base_cov
+    retries = 0
+    while retries < 5:
+        url = f"https://api.codecov.io/api/v2/github/{org}/repos/{repo}/pulls/{pr}/"
+        print(f"Getting codecov.io status from {url}")
+        with urlopen(url) as response:
+            response_content = response.read().decode("utf-8")
+            data = json.loads(response_content)
+            print(data, file=sys.stdout)
+            base_cov = 0.0
+            head_cov = 0.0
+            if data["base_totals"] is not None:
+                base_cov = data["base_totals"]["coverage"]
+            if data["head_totals"] is not None:
+                head_cov = data["head_totals"]["coverage"]
+            delta_coverage = head_cov - base_cov
+        if head_cov != 0.0:
+            break
+        retries += 1
+        delay = retries * 30
+        print(
+            f"::notice::Codecov API returned no head_totals, we will retry the request in {delay} seconds."
+        )
+        time.sleep(delay)
 
     msg = f"{abs(delta_coverage):.2f}% ({base_cov:.2f}% on base -> {head_cov:.2f}% on head).\n"
     msg += f"See https://app.codecov.io/gh/{org}/{repo}/pull/{pr} for details."
